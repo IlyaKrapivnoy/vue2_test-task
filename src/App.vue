4f28c17b33a58024034ba842a28274f158cbe343
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2023-09-09 00:05:31 +0100

    fetch data

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,56 @@
         Soccer Stats
       </h1>
     </div>
+    <table>
+      <thead>
+        <tr>
+          <th></th>
+          <th></th>
+          <th></th>
+          <th>Form</th>
+          <th>GP</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(team, index) in tableData" :key="index">
+          <td>{{ team.strTeam }}</td>
+          <td>{{ team.intPlayed }}</td>
+          <td>{{ team.intWin }}</td>
+          <td>{{ team.intDraw }}</td>
+          <td>{{ team.intLoss }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'App',
-  components: {}
+  components: {},
+  data() {
+    return {
+      tableData: []
+    };
+  },
+  methods: {
+    logTeam(team) {
+      console.log(team);
+    }
+  },
+  mounted() {
+    axios
+      .get(
+        'https://www.thesportsdb.com/api/v1/json/3/lookuptable.php?l=4328&s=2020-2021'
+      )
+      .then(response => {
+        this.tableData = response.data.table;
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }
 };
 </script>
-
-<style></style>
